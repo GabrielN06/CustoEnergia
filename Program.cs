94e42d3893099cb9afafd3fe32903980e66925ee
Gabriel <gabrielnunesdacostaanselmo@gmail.com>
2025-03-24 10:22:47 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿double consumo, horas, custo, custoMes;
+
+Console.WriteLine("--- Custo de Energia ---");
+
+Console.Write($"Consumo do aparelho (em kWh/mês): ");
+consumo = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Horas de uso por dia...............: ");
+horas = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Custo da energia (em R$/kWh).......: ");
+custo = Convert.ToDouble(Console.ReadLine());
+
+custoMes = consumo * horas * custo;
+
+Console.Write($"Custo estimado: {custoMes:F2}");
+
+
+
+
